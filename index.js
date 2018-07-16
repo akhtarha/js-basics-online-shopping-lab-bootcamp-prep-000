3a9d813924caf857b2f7f45ebbbc4cd3058adfbe
Hassan A <has_is_ere@hotmail.com>
2018-07-16 12:45:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ function removeFromCart(item) {
 	for (var i = 0; i < cart.length; i++){
   	if (cart[i].itemName === item){
     	cart.splice(i, 1)
-      console.log(cart)
+      return cart
     }else if (i === cart.length-1 && cart[i].itemName != item){
-    	console.log("That item is not in your cart.")
+    	return "That item is not in your cart."
     }
   }
 }
