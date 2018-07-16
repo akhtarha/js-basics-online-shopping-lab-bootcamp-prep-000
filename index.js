7847615576477b056d99c80b11451065a9ebd9de
Hassan A <has_is_ere@hotmail.com>
2018-07-16 13:03:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,14 +39,17 @@ function total() {
 }
 
 function removeFromCart(item) {
-	for (var i = 0; i < cart.length; i++){
-  	if (cart[i].itemName === item){
-    	cart.splice(i, 1)
-      return cart
-    }else if (i === cart.length-1 && cart[i].itemName != item){
-    	return "That item is not in your cart."
-    }
-  }
+	if (cart.length === 0){
+  	return "That item is not in your cart."
+  } else {
+  	for (var i = 0; i < cart.length; i++){
+  		if (cart[i].itemName === item){
+    		cart.splice(i, 1)
+    	}else if (i === cart.length-1 && cart[i].itemName != item){
+    		return "That item is not in your cart."
+  		}
+		}
+	}
 }
 
 function placeOrder(cardNumber) {
