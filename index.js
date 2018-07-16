90860757147ee325e105a93b3f92cb8ca2758d82
Hassan A <has_is_ere@hotmail.com>
2018-07-16 12:45:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,7 +38,16 @@ function total() {
   return total
 }
 
-
+function removeFromCart(item) {
+	for (var i = 0; i < cart.length; i++){
+  	if (cart[i].itemName === item){
+    	cart.splice(i, 1)
+      console.log(cart)
+    }else if (i === cart.length-1 && cart[i].itemName != item){
+    	console.log("That item is not in your cart.")
+    }
+  }
+}
 
 function placeOrder(cardNumber) {
   // write your code here
